3d75526a5d0cf9d00c1382a7148dfb8868e3d10c
Daewoong Ko <kodw4284@gmail.com>
2022-01-06 07:03:48 -0500

    feat: disable smooth scroll on mobile devices

--- src/lib/smoothScroll.ts
+++ src/lib/smoothScroll.ts
@@ -1,7 +1,10 @@
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Scrollbar from 'smooth-scrollbar';
+import { isMobile } from 'react-device-detect';
 
 export function applySmoothScroll(scrollTrigger: typeof ScrollTrigger, scrollBar: typeof Scrollbar) {
+  if(isMobile) return;
+  
   const scroller = document.querySelector("html") as HTMLElement;
   const bodyScrollBar = scrollBar.init(scroller, { damping: 0.1, delegateTo: document, alwaysShowTracks: true });
   
