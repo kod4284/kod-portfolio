2018cf6328c930b5fe2033fe1a04ffe9c724f70d
Daewoong Ko <kodw4284@gmail.com>
2022-01-14 17:43:10 -0500

    feat: add animation to MajorSkills page

--- src/components/major-skills/MajorSkills.tsx
+++ src/components/major-skills/MajorSkills.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 
 import TsIcon from 'assets/photos/ts.png';
 import JsIcon from 'assets/photos/js.png';
@@ -9,7 +10,7 @@ import NodeIcon from 'assets/photos/node-js.png';
 import SpringIcon from 'assets/photos/spring.png';
 import FlutterIcon from 'assets/photos/flutter.png';
 
-import { TitleText } from 'components/commons/styles';
+import { OverFlowContainer, TitleText } from 'components/commons/styles';
 import {
   Container,
   ContentContainer,
@@ -20,34 +21,112 @@ import {
 } from './MajorSkills.style';
 
 function MajorSkills() {
+  const titleRef = useRef(null);
+  const tsRef = useRef(null);
+  const jsRef = useRef(null);
+  const javaRef = useRef(null);
+  const pythonRef = useRef(null);
+  const reactRef = useRef(null);
+  const nodeRef = useRef(null);
+  const springRef = useRef(null);
+  const flutterRef = useRef(null);
+  const endTextRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(titleRef.current, {
+      scrollTrigger: {
+        trigger: titleRef.current,
+        start: "top bottom"
+      },
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    })
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: tsRef.current,
+        start: "top bottom"
+      }
+    });
+    tl.from(tsRef.current, {
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    }, "<0.1")
+    .from(jsRef.current, {
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    }, "<0.1")
+    .from(javaRef.current, {
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    }, "<0.1")
+    .from(pythonRef.current, {
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    }, "<0.1")
+    .from(reactRef.current, {
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    }, "<0.1")
+    .from(nodeRef.current, {
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    }, "<0.2")
+    .from(springRef.current, {
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    }, "<0.1")
+    .from(flutterRef.current, {
+      duration: 1,
+      ease: "power3.out",
+      yPercent: 200
+    }, "<0.1")
+    gsap.from(endTextRef.current, {
+      scrollTrigger: {
+        trigger: endTextRef.current,
+        start: "top bottom"
+      },
+      duration: 2,
+      opacity: 0
+    })
+  }, []); 
   const icons = [
-    TsIcon,
-    JsIcon,
-    JavaIcon,
-    PythonIcon,
-    ReactIcon,
-    NodeIcon,
-    SpringIcon,
-    FlutterIcon
+    {ref: tsRef ,icon: TsIcon},
+    {ref: jsRef, icon: JsIcon},
+    {ref: javaRef, icon: JavaIcon},
+    {ref: pythonRef, icon: PythonIcon},
+    {ref: reactRef, icon: ReactIcon},
+    {ref: nodeRef, icon: NodeIcon},
+    {ref: springRef, icon: SpringIcon},
+    {ref: flutterRef, icon: FlutterIcon}
   ];
   return (
     <Container>
-      <TitleText>Major Skills</TitleText>
+      <TitleText ref={titleRef}>Major Skills</TitleText>
       <ContentContainer>
         <SkillIcons icons={icons} />
-        <Text>And More ...</Text>
+        <Text ref={endTextRef}>And More ...</Text>
       </ContentContainer>
     </Container>
   )
 }
 
-function SkillIcons({ icons }: { icons: string[] }) {
+function SkillIcons({ icons }: { icons: { ref: React.RefObject<HTMLImageElement>, icon: string}[] }) {
   return (
     <FlexContainer>
       {
-        icons.map((icon, i) => (
+        icons.map(({ref, icon}, i) => (
         <IconContainer key={i}>
-          <Icon src={icon} />
+          <OverFlowContainer>
+            <Icon ref={ref} src={icon} />
+          </OverFlowContainer>
         </IconContainer>
         ))
       }
